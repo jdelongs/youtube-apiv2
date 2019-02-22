6171aa21f372b81ffba1766d4d2c26b2faac0c6d
John Delong <JDelong@caranciconsulting.com>
2019-02-22 14:59:00 -0500

    added video detail and styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,42 @@ import React from 'react';
 import youtube from '../apis/youtube'; 
 import SearchBar from './SearchBar'; 
 import VideoList from './VideoList'; 
+import VideoDetail from './VideoDetail'; 
 class App extends React.Component {
-    state = { videos: [] }; 
+    state = { 
+        videos: [], 
+        selectedVideo: null 
+    }; 
     onSearchSubmit = async (term) => {
-
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         }); 
-
-        this.setState({ videos: response.data.items }); 
+        this.setState({ 
+            videos: response.data.items, 
+            selectedVideo: null 
+        
+        });
     } 
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});  
+    }
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSearchSubmit={this.onSearchSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                       
+                        <div className="eleven wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                        
+                    </div>
+                </div>
+               
             </div>
         )
     }; 
